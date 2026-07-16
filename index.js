c98906d4b19a7673f85f3ed33292e42055c1664e
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-16 20:56:53 +0600

    practice nodemon

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = process.env.Port || 8000;
 app.get('/', (req, res) => {
     res.send('hello');
 })
+app.get('/user', (req, res) => {
+    res.send('hello user');
+})
 
 app.listen(port, () => {
     console.log("server is running")
